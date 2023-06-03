a2a6ca5ed3ae062ee4aea1c371d67b74bb9d90b2
ian <git@audeo.ltd>
2023-06-04 08:57:51 +0900

    create Feed component and it's header

--- app/page.js
+++ app/page.js
@@ -1,3 +1,4 @@
+import Feed from '@/components/Feed'
 import Sidebar from '@/components/Sidebar'
 import Image from 'next/image'
 
@@ -7,7 +8,7 @@ export default function Home() {
         
        <Sidebar />
 
-        
+      <Feed />
     </main>
   )
 }
