03b1c0291d173b154010010f301761b8dcf47ad3
ian <git@audeo.ltd>
2023-06-04 21:21:41 +0900

    create the widgets components and the search bar

--- app/page.js
+++ app/page.js
@@ -1,14 +1,17 @@
 import Feed from '@/components/Feed'
 import Sidebar from '@/components/Sidebar'
+import Widgets from '@/components/Widgets'
 import Image from 'next/image'
 
 export default function Home() {
   return (
     <main className="flex min-h-screen max-w-7xl mx-auto">
         
-       <Sidebar />
+      <Sidebar />
 
       <Feed />
+
+      <Widgets />
     </main>
   )
 }
